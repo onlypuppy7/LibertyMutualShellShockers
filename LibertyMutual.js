4d6cdbd151582130ad9f404cef35fb7977292495
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2024-02-11 18:31:34 +0000

    new method

--- LibertyMutual.js
+++ LibertyMutual.js
@@ -2,20 +2,21 @@
 // @name         LibertyMutualV1 For Shell Shockers
 // @namespace    https://github.com/onlypuppy7/LibertyMutualShellShockers/
 // @license      GPL-3.0
-// @version      1.0.6
+// @version      1.1.0
 // @author       onlypuppy7
 // @description  FOSS ESP, Tracers and Aimbot. Hold right mouse button to aimlock.
 // @match        https://shellshock.io/*
 // @grant        none
 // @run-at       document-start
 // @icon         https://github.com/onlypuppy7/LibertyMutualShellShockers/blob/main/scripticon.jpg?raw=true
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
 // ==/UserScript==
 
 //Usage: Hold right mouse button to aimlock
+//This script is more of a template than a functioning tool. If you're modifying this, you can add a GUI to start!
 
 (function () {
     //Config: if you want to turn off esp, you can.
-    //This script is more of a template than a functioning tool. If you're modifying this, you can add a GUI to start!
 
     const enableESP=true; //turn to false for off
     const enableTracers=true; //turn to false for off
@@ -85,17 +86,39 @@
         // Property not found
         return null;
     };
-
-    //Credit for idea to use regexes to dynamically create the injected script: helloworld (although it is not a new concept)
-    const applyLibertyMutual = function(js) {
-        const vars=[];
-        let injectionString="";
-        const getVar=function(name,regex){
-            const varName=eval(new RegExp(regex)+`.exec(js)[1]`);
-            vars[name]=varName;
-            injectionString=injectionString+name+": "+varName+",";
-            console.log('%cFound var! Saved '+varName+' as '+name, 'color: green; font-weight: bold;');
+    const fetchTextContent = function(url) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false); // Make the request synchronous
+        xhr.send();
+        if (xhr.status === 200) {
+            return xhr.responseText;
+        } else {
+            console.error("Error fetching text content. Status:", xhr.status);
+            return null;
         };
+    };
+
+    const applyLibertyMutual = function(js) {
+
+        let hash = CryptoJS.SHA256(js).toString(CryptoJS.enc.Hex);
+        let clientKeys;
+        onlineClientKeys = fetchTextContent("https://raw.githubusercontent.com/StateFarmNetwork/client-keys/main/libemrtymutual_"+hash+".json"); //credit: me :D
+
+        if (onlineClientKeys == "value_undefined" || onlineClientKeys == null) {
+            let userInput = prompt('Valid keys could not be retrieved online. Enter keys if you have them. Join the StateFarm Network Discord server to generate keys! https://discord.gg/HYJG3jXVJF', '');
+            if (userInput !== null && userInput !== '') {
+                alert('Aight, let\'s try this. If it is invalid, it will just crash.');
+                clientKeys = JSON.parse(userInput);
+            } else {
+                alert('You did not enter anything, this is gonna crash lmao.');
+            };
+        } else {
+            clientKeys = JSON.parse(onlineClientKeys);
+        };
+
+        H = clientKeys.vars;
+
+        let injectionString="";
         
         const modifyJS = function(find,replace) {
             let oldJS = js;
@@ -108,30 +131,19 @@
         };
 
         console.log('%cATTEMPTING TO START LIBERTYMUTUAL', 'color: magenta; font-weight: bold; font-size: 1.5em; text-decoration: underline;');
-
-        console.log('%cLIBERTYMUTUAL INJECTION STAGE 1: GATHER VARS', 'color: yellow; font-weight: bold; font-size: 1.2em; text-decoration: underline;');
-        //Credit for idea to gather vars: helloworld, PacyTense. Also common sense.
-        getVar("PLAYERS", '([a-zA-Z]+)\\[[a-zA-Z]+\\]\\.hp=100');
-        getVar("MYPLAYER", '\\),([a-zA-Z]+)\\.pitch=Math\\.clamp\\(');
-        getVar("BABYLONJS", '\\),this\\.range=([a-zA-Z]+)\\.');
-
-        console.log('%cLIBERTYMUTUAL INJECTION STAGE 2: INJECT VAR RETRIEVAL FUNCTION AND MAIN LOOP', 'color: yellow; font-weight: bold; font-size: 1.2em; text-decoration: underline;');
-        match=js.match(/\.engine\.([a-zA-Z]+)\(\(function\(\)\{!/);
-        console.log(match);
-        modifyJS('.engine.'+match[1]+'((function(){',`.engine.${match[1]}((function(){if(window["${functionNames.retrieveFunctions}"]({${injectionString}},true)){return};`);
+        for (let name in H) {
+            deobf = H[name];
+            injectionString=`${injectionString}${name}: (() => { try { return ${deobf}; } catch (error) { return "value_undefined"; } })(),`;
+        };
+        console.log(injectionString);
+        console.log('%cLIBERTYMUTUAL INJECTION: INJECT VAR RETRIEVAL FUNCTION AND MAIN LOOP', 'color: yellow; font-weight: bold; font-size: 1.2em; text-decoration: underline;');
+        modifyJS(H.SCENE+'.render',`window["${functionNames.retrieveFunctions}"]({${injectionString}},true)||${H.SCENE}.render`);
         console.log('%cSuccess! Variable retrieval and main loop hooked.', 'color: green; font-weight: bold;');
-
-        match=js.match(/&&!([a-zA-Z]+)&&function\(\)\{if\(/);
-        modifyJS(`{if(${match[1]})`,`{if(true)`);
+        modifyJS(`{if(${H.CULL})`,`{if(true)`);
         console.log('%cSuccess! Cull inhibition hooked.', 'color: green; font-weight: bold;');
-
-        H.playing = js.match(/this\.hp=[a-zA-Z]+\.hp,this\.([a-zA-Z]+)=[a-zA-Z]+\.[a-zA-Z]+,this/)[1];
-        H.MeshBuilder = js.match(/\.([a-zA-Z]+)\.CreateLineSystem\("/)[1];
-        H.CreateLines = js.match(/\.([a-zA-Z]+)\("yPosMesh",\{points/)[1];
-
-        console.log(H);
-
         modifyJS("Not playing in iframe", "LIBERTYMUTUAL ACTIVE!");
+        // console.log(js);
+        console.log(H);
         return js;
     };
 
@@ -141,16 +153,16 @@
         globalSS = ss;
 
         ss.BABYLONJS.Vector3 = ss.MYPLAYER.constructor.v1.constructor;
-        H.actor = findKeyWithProperty(ss.MYPLAYER,"mesh");
+        H.actor = findKeyWithProperty(ss.MYPLAYER,H.mesh);
 
         let TARGETED;
         let CROSSHAIRS=new ss.BABYLONJS.Vector3();
-        CROSSHAIRS.copyFrom(ss.MYPLAYER[H.actor].mesh.position);
-        const horizontalOffset = Math.sin(ss.MYPLAYER[H.actor].mesh.rotation.y);
-        const verticalOffset = Math.sin(-ss.MYPLAYER.pitch);
+        CROSSHAIRS.copyFrom(ss.MYPLAYER[H.actor][H.mesh].position);
+        const horizontalOffset = Math.sin(ss.MYPLAYER[H.actor][H.mesh].rotation.y);
+        const verticalOffset = Math.sin(-ss.MYPLAYER[H.pitch]);
         CROSSHAIRS.x+=horizontalOffset;
         CROSSHAIRS.y+=verticalOffset+0.4;
-        CROSSHAIRS.z+=Math.cos(ss.MYPLAYER[H.actor].mesh.rotation.y);
+        CROSSHAIRS.z+=Math.cos(ss.MYPLAYER[H.actor][H.mesh].rotation.y);
 
         const timecode=Date.now();
         let minValue=99999;
@@ -158,7 +170,7 @@
             if (PLAYER) {
                 PLAYER.timecode=timecode;
                 //Partial credit for enemy player filtering: PacyTense. Also just common sense.
-                if (((PLAYER!==ss.MYPLAYER)&&((!ss.MYPLAYER.team)||(PLAYER.team!==ss.MYPLAYER.team)))) {
+                if ((PLAYER!==ss.MYPLAYER) && ((ss.MYPLAYER.team==0)||(PLAYER.team!==ss.MYPLAYER.team))) {
                     //ESP CODE
                     if ((!PLAYER.generatedESP)) {
                         //Credit for box from lines code: AI. ChatGPT prompt: "how can i create a box out of lines in babylon.js?"
@@ -180,14 +192,15 @@
                             lines.push([vertices[i + 4], vertices[(i + 1) % 4 + 4]]);
                             lines.push([vertices[i], vertices[i + 4]]);
                         };
-                        const box = ss.BABYLONJS[H.MeshBuilder].CreateLineSystem('boxLines', { lines }, PLAYER[H.actor].scene);
+                        const box = ss.BABYLONJS[H.MeshBuilder].CreateLineSystem(getScrambled(), { lines }, PLAYER[H.actor].scene);
                         //ChatGPT prompt: "how can i make an object anchored to another object, change its color, and have it render on top of everything else? babylon.js"
                         box.color = new ss.BABYLONJS.Color3(1, 1, 1);
                         box.renderingGroupId = 1;
-                        box.parent=PLAYER[H.actor].mesh;
+                        box.parent=PLAYER[H.actor][H.mesh];
                         //TRACER LINES
-                        const tracers=ss.BABYLONJS[H.MeshBuilder][H.CreateLines]("tracerLines", { points: [PLAYER[H.actor].mesh.position, CROSSHAIRS] }, PLAYER[H.actor].scene);
+                        const tracers=ss.BABYLONJS[H.MeshBuilder][H.CreateLines]('lines', { points: [PLAYER[H.actor][H.mesh].position, CROSSHAIRS] }, PLAYER[H.actor].scene);
                         tracers.color=new ss.BABYLONJS.Color3(1, 1, 1);
+                        tracers.alwaysSelectAsActiveMesh = true;
                         tracers.renderingGroupId=1;
                         
                         PLAYER.box=box;
@@ -196,29 +209,32 @@
                         ESPArray.push([box,tracers,PLAYER]);
                     };
                     //update the lines
-                    PLAYER.tracers.setVerticesData(ss.BABYLONJS.VertexBuffer.PositionKind, [CROSSHAIRS.x, CROSSHAIRS.y, CROSSHAIRS.z, PLAYER[H.actor].mesh.position.x, PLAYER[H.actor].mesh.position.y, PLAYER[H.actor].mesh.position.z]);
-
+                    PLAYER.tracers.setVerticesData(ss.BABYLONJS.VertexBuffer.PositionKind, [CROSSHAIRS.x, CROSSHAIRS.y, CROSSHAIRS.z, PLAYER[H.actor][H.mesh].position.x, PLAYER[H.actor][H.mesh].position.y, PLAYER[H.actor][H.mesh].position.z]);
+                    console.log(CROSSHAIRS)
                     PLAYER.box.visibility=enableESP;
                     PLAYER.tracers.visibility=(PLAYER[H.playing]&&enableTracers);
 
                     //AIMBOT CODE
                     //Credit: This section is mostly common sense, and could be made by most decent programmers. It is still worth mentioning PacyTense used a functionally equivalent thing similar to this this before me 4 years ago.
-                    const distance=ss.BABYLONJS.Vector3.Distance(ss.MYPLAYER,PLAYER);
+                    const distance=Math.hypot(PLAYER[H.x]-ss.MYPLAYER[H.x], PLAYER[H.y]-ss.MYPLAYER[H.y], PLAYER[H.z]-ss.MYPLAYER[H.z]);
+
+                    if (RMB) {console.log(PLAYER.name, PLAYER.team, Date.now())}
+
                     if (distance<minValue) {
                         TARGETED=PLAYER;
                         minValue=distance;
                     };
                 };
             };
-            if (RMB&&TARGETED&&TARGETED[H.playing]) {
+            if (RMB && TARGETED && TARGETED[H.playing]) {
                 //3D maths
                 const directionVector={
-                    x: TARGETED.x-ss.MYPLAYER.x,
-                    y: TARGETED.y-ss.MYPLAYER.y,
-                    z: TARGETED.z-ss.MYPLAYER.z,
+                    [H.x]: TARGETED[H.x]-ss.MYPLAYER[H.x],
+                    [H.y]: TARGETED[H.y]-ss.MYPLAYER[H.y]-0.05,
+                    [H.z]: TARGETED[H.z]-ss.MYPLAYER[H.z],
                 };
-                ss.MYPLAYER.yaw=F.calculateYaw(directionVector);
-                ss.MYPLAYER.pitch=F.calculatePitch(directionVector);
+                ss.MYPLAYER[H.yaw]=F.calculateYaw(directionVector);
+                ss.MYPLAYER[H.pitch]=F.calculatePitch(directionVector);
             };
         });
         for ( let i=0;i<ESPArray.length;i++) {
@@ -233,9 +249,9 @@
     });
     createAnonFunction("setPrecision",function (value) { return Math.floor(value * 8192) / 8192 }); //required precision
     createAnonFunction("calculateYaw",function (pos) {
-        return F.setPrecision(Math.mod(Math.atan2(pos.x,pos.z), Math.PI2));
+        return F.setPrecision(Math.mod(Math.atan2(pos[H.x],pos[H.z]), Math.PI2));
     });
     createAnonFunction("calculatePitch",function (pos) {
-        return F.setPrecision(-Math.atan2(pos.y,Math.hypot(pos.x,pos.z))%1.5);
+        return F.setPrecision(-Math.atan2(pos[H.y],Math.hypot(pos[H.x],pos[H.z]))%1.5);
     });
 })();
