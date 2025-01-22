18ee34571ad0d82a65f0e01c2e88057affa2c91c
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2025-01-22 12:41:08 +0000

    version to 1.3.2

--- LibertyMutual.js
+++ LibertyMutual.js
@@ -2,7 +2,7 @@
 // @name         Shell Shockers Basic Aimbot + ESP: LibertyMutualV1
 // @namespace    https://github.com/onlypuppy7/LibertyMutualShellShockers/
 // @license      GPL-3.0
-// @version      1.3.1
+// @version      1.3.2
 // @author       onlypuppy7
 // @description  UPDATED FOR 0.50.0! Fed up of a popular script injecting ads into your game? Need a simple script to modify or use? FOSS ESP, Tracers and Aimbot. Hold right mouse button to aimlock.
 // @match        https://shellshock.io/*
